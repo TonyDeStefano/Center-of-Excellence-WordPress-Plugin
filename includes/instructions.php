051793221f99e8f9f6d8e4d399f623f7a6cdba30
Tony DeStefano <spokanetony@gmail.com>
2016-12-15 08:59:16 -0800

    I suppose having some instructions on the instructions page would be nice.

--- includes/instructions.php
+++ includes/instructions.php
@@ -12,4 +12,8 @@
 		Center Of Excellence Plugin <?php _e( 'Instructions', 'coe' ); ?>
 	</h1>
 
+    <p>Use this shortcode to insert the recent graduates system onto any page:</p>
+
+    [coe display="recent_grads"]
+
 </div>
